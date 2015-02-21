4d2dd691123114bada5b921505d747d6dfac3f12
ElJerry <kanegerardo_@hotmail.com>
2015-02-21 12:18:29 -0600

    Modificacion
    Le agregue la funcion de crear la poblacion inicial y aparte puse una
    funcion para ver la poblacion

--- Proyecto_3.cpp
+++ Proyecto_3.cpp
@@ -6,9 +6,39 @@
 		Juan Jose Tovar
 		Luis Gerardo Hurtado
 */
+#include <iostream>
+#include <windows.h>
 
 using namespace std;
 
-int main(){
+// Vector para la poblacion inicial
+int individuo[100][16];
 
+void poblacionInicial(){
+	srand(time(NULL));
+
+	for(int i=0 ; i<100 ; i++){
+		for(int j = 0; j < 16 ; j++){
+			individuo[i][j] = rand() % 2;
+			//cout << individuo[i][j];
+		}
+	}
+}
+
+void mostrarIndividuos(){
+	for(int i=0 ; i<100 ; i++){
+
+		printf("Individuo %d:\t [",i+1);
+
+		for(int j=0 ; j<16 ; j++){
+			printf("%d,",individuo[i][j]);
+		}
+		printf("]\n");
+	}
+}
+
+int main(){
+	poblacionInicial();
+	mostrarIndividuos();
+	system("pause");
 }
